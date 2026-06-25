2cb4cfe1665ab024ebb8455ce47e4637ec0225ae
Shell <shellington@users.noreply.github.com>
2026-06-25 10:06:47 +0100

    image handling

--- data/projects.ts
+++ data/projects.ts
@@ -1,88 +1,241 @@
 export type Project = {
-	name: string
-	description: string
-	image: string
-	tags: string[]
-	live?: string
-	github?: string
-}
+  name: string;
+  description: string;
+  image: string;
+  tags: string[];
+  live?: string;
+  github?: string;
+};
 
 export const projects: Project[] = [
-	{
-		name: 'Lloyds',
-		description: 'Short description of what this project does.',
-		image: '/images/lloyds.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://www.lloydsbankinggrouptalent.com/'
-	},
-	{
-		name: 'Uber',
-		description: 'Short description of what this project does.',
-		image: '/images/uber.jpg',
-		tags: ['React', 'Next.js', 'TypeScript', 'Tailwind', 'Git', 'GitHub', 'Umbraco', 'Headless CMS', 'Content Delivery API', 'pnpm', 'Figma'],
-		live: 'https://jobs.uber.com/en/'
-	},
-	{
-		name: 'Salesforce',
-		description: 'Short description of what this project does.',
-		image: '/images/salesforce.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://www.salesforce.com/company/careers/'
-	},
-	{
-		name: 'Dominos',
-		description: 'Short description of what this project does.',
-		image: '/images/dominos.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://jobs.dominos.com/us/'
-	},
-	{
-		name: 'Criteo',
-		description: 'Short description of what this project does.',
-		image: '/images/criteo.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://careers.criteo.com/en/'
-	},
-	{
-		name: 'Grab',
-		description: 'Short description of what this project does.',
-		image: '/images/grab.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://www.grab.careers/en/'
-	},
-	{
-		name: 'Xero',
-		description: 'Short description of what this project does.',
-		image: '/images/xero.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://careers.xero.com/'
-	},
-	{
-		name: 'Nutanix',
-		description: 'Short description of what this project does.',
-		image: '/images/nutanix.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'SVG Animnation', 'Figma'],
-		live: 'https://careers.nutanix.com/en/'
-	},
-	{
-		name: 'Canva',
-		description: 'Short description of what this project does.',
-		image: '/images/canva.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://www.lifeatcanva.com/en/'
-	},
-	{
-		name: 'Verizon',
-		description: 'Short description of what this project does.',
-		image: '/images/verizon.jpg',
-		tags: ['React', 'Next.js', 'TypeScript', 'Tailwind', 'Git', 'GitHub', 'Umbraco', 'Headless CMS', 'Content Delivery API', 'pnpm', 'Figma'],
-		live: 'https://mycareer.verizon.com/'
-	},
-	{
-		name: 'Box',
-		description: 'Short description of what this project does.',
-		image: '/images/box.jpg',
-		tags: ['Sass/SCSS', 'JavaScript', 'HTML5', 'Bootstrap', 'Umbraco', 'Razor Syntax', 'GSAP', 'npm', 'Gulp', 'Git', 'GitHub', 'Figma'],
-		live: 'https://careers.box.com/en/'
-	}
-]
+  {
+    name: "Lloyds",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377166/lloyds_aelacg.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://www.lloydsbankinggrouptalent.com/",
+  },
+  {
+    name: "Uber",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377167/uber_aimimq.jpg",
+    tags: [
+      "React",
+      "Next.js",
+      "TypeScript",
+      "Tailwind",
+      "Git",
+      "GitHub",
+      "Umbraco",
+      "Headless CMS",
+      "Content Delivery API",
+      "pnpm",
+      "Figma",
+    ],
+    live: "https://jobs.uber.com/en/",
+  },
+  {
+    name: "Salesforce",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377167/salesforce_cbovn0.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://www.salesforce.com/company/careers/",
+  },
+  {
+    name: "Dominos",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377166/dominos_u4iupb.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://jobs.dominos.com/us/",
+  },
+  {
+    name: "Criteo",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377167/criteo_hj8cv2.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://careers.criteo.com/en/",
+  },
+  {
+    name: "Grab",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377166/grab_rwaf40.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://www.grab.careers/en/",
+  },
+  {
+    name: "Xero",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377166/xero_q0rseg.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://careers.xero.com/",
+  },
+  {
+    name: "Nutanix",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377166/nutanix_bfctrm.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "SVG Animnation",
+      "Figma",
+    ],
+    live: "https://careers.nutanix.com/en/",
+  },
+  {
+    name: "Canva",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377167/canva_kx45w0.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://www.lifeatcanva.com/en/",
+  },
+  {
+    name: "Verizon",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377167/verizon_z6b1pn.jpg",
+    tags: [
+      "React",
+      "Next.js",
+      "TypeScript",
+      "Tailwind",
+      "Git",
+      "GitHub",
+      "Umbraco",
+      "Headless CMS",
+      "Content Delivery API",
+      "pnpm",
+      "Figma",
+    ],
+    live: "https://mycareer.verizon.com/",
+  },
+  {
+    name: "Box",
+    description: "Short description of what this project does.",
+    image:
+      "https://res.cloudinary.com/dcjf1g82f/image/upload/f_auto/q_auto/v1782377166/box_zfjmqs.jpg",
+    tags: [
+      "Sass/SCSS",
+      "JavaScript",
+      "HTML5",
+      "Bootstrap",
+      "Umbraco",
+      "Razor Syntax",
+      "GSAP",
+      "npm",
+      "Gulp",
+      "Git",
+      "GitHub",
+      "Figma",
+    ],
+    live: "https://careers.box.com/en/",
+  },
+];
